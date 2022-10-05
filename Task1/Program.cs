4f9e836b2c5b61fc2487d6286a9b734a8d35fbc7
Emil-87 <emil.pegas@gmail.com>
2022-10-06 00:50:18 +0600

    задача 2

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать задачу, которая на вход принимает два числа и найти мин и макс.
+Console.Write("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(b > a) max = b;
+Console.WriteLine(max);
+
+//a = 5 b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> = -3
